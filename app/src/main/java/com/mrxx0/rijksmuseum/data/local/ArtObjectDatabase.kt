3345eebff08ef70588431f17cda3b8edacf00cdb
mrxx0 <chcoutur@student.42.fr>
2023-12-20 17:38:34 +0100

    Fix typo to extend from RoomDatabase() in ArtObjectDatabase
    Signed-off-by: mrxx0 <chcoutur@student.42.fr>

--- app/src/main/java/com/mrxx0/rijksmuseum/data/local/ArtObjectDatabase.kt
+++ app/src/main/java/com/mrxx0/rijksmuseum/data/local/ArtObjectDatabase.kt
@@ -1,11 +1,12 @@
 package com.mrxx0.rijksmuseum.data.local
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
 
 @Database(
     entities = [ArtObjectEntity::class],
     version = 1
 )
-abstract class ArtObjectDatabase {
+abstract class ArtObjectDatabase : RoomDatabase() {
     abstract val dao: ArtObjectDao
 }
